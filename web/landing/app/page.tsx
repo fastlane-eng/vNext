61bd38feab769f244ad55e9a994461d54724fc45
fastlane-eng <fast.lane0811@gmail.com>
2025-11-23 08:44:44 -0800

    [FLX] Add landing page.tsx

---
+++ web/landing/app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to FastLane vNext</h1>
+      <p>This is the enterprise landing page. More components will be added here.</p>
+    </main>
+  );
+}
