92d519a761135935f406dff6359471a0e30d2a74
fastlane-eng <fast.lane0811@gmail.com>
2025-11-23 07:03:15 -0800

    [FLX] Add Kpi domain model

---
+++ src/FastLane.Core/Models/Kpi.cs
@@ -0,0 +1,9 @@
+namespace FastLane.Core.Models
+{
+    public class Kpi
+    {
+        public string Name { get; set; } = string.Empty;
+        public decimal Value { get; set; }
+        public string Unit { get; set; } = string.Empty;
+    }
+}
